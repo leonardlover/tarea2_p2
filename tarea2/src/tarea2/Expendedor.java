9e02ba4d7e78a507e57fe9151907d32f31cf4a5a
Leonardo Lovera <leonard.lover@gmail.com>
2022-10-31 00:15:48 -0300

    Bug fixes

--- tarea2/src/tarea2/Expendedor.java
+++ tarea2/src/tarea2/Expendedor.java
@@ -17,6 +17,8 @@ class Expendedor {
         sprite = new Deposito();
         fanta = new Deposito();
 
+        vuelto = new DepositoVuelto();
+
         precio = 100 * precioBebidas; // debe ser un multiplo de 100
 
         for(int i = 0; i < numBebidas; i++) {
@@ -27,37 +29,70 @@ class Expendedor {
     }
 
     public Bebida comprarBebida(Moneda m, int cual) throws NoHayBebidaException, PagoInsuficienteException, PagoIncorrectoException {
-        Bebida r = null;
-
-        if(m == null) {
-            throw new PagoIncorrectoException();
-        }
-        else if(m.getValor() < precio) {
-            throw new PagoInsuficienteException();
-        }
-        else {
-            switch(num) {
-                case 1: // cocacola
-                    if(coca.getSize() == 0) {
-                        throw new NoHayBebidaException();
-                    }
-                    return coca.getBebida();
-                    
-                case 2: // sprite
-                    if(sprite.size() == 0) {
-                        throw new NoHayBebidaException();
-                    }
-                    return sprite.getBebida();
-                
-                case 3: // fanta
-                    if(fanta.getSize() == 0) {
-                        throw new NoHayBebidaException();
-                    }
-                    return fanta.getBebida();
-                
-                default:
-                    throw new NoHayBebidaException();
+        try {
+            if (m == null) {
+                throw new PagoIncorrectoException();
             }
+            else if (m.getValor() < precio) {
+                vuelto.addMoneda(m);
+                throw new PagoInsuficienteException();
+            }
+            else {
+                switch (num) {
+                    case 1: // cocacola
+                        if (coca.getSize() == 0) {
+                            vuelto.addMoneda(m);
+                            throw new NoHayBebidaException();
+                        }
+
+                        for (int i = 0; i < (precio - m.getValor()) / 100; i++) {
+                            vuelto.addMoneda(new Moneda100());
+                        }
+
+                        return coca.getBebida();
+                        
+                    case 2: // sprite
+                        if (sprite.size() == 0) {
+                            vuelto.addMoneda(m);
+                            throw new NoHayBebidaException();
+                        }
+
+                        for (int i = 0; i < (precio - m.getValor()) / 100; i++) {
+                            vuelto.addMoneda(new Moneda100());
+                        }
+
+                        return sprite.getBebida();
+                    
+                    case 3: // fanta
+                        if (fanta.getSize() == 0) {
+                            vuelto.addMoneda(m);
+                            throw new NoHayBebidaException();
+                        }
+
+                        for (int i = 0; i < (precio - m.getValor()) / 100; i++) {
+                            vuelto.addMoneda(new Moneda100());
+                        }
+
+                        return fanta.getBebida();
+                    
+                    default:
+                        vuelto.addMoneda(m);
+                        throw new NoHayBebidaException();
+                }
+            }
+        }
+
+        catch (PagoIncorrectoException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        catch (PagoInsuficienteException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        catch (NoHayBebidaException ex) {
+            System.out.println(ex.getMessage());
+            return null;
         }
     }
 
